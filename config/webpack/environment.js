a31073aafbbbea82d08fe74df077c6ad2560b58d
kurosiroke <129666467+kurosiroke@users.noreply.github.com>
2023-10-04 12:44:30 +0000

    update

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
+const webpack =require('webpack')
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
+    Swiper: 'swiper/swiper-bundle'
   })
 )
+
 
 module.exports = environment
